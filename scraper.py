840edc457ddbc586834bd5033bc6a25564f9b1d5
Sayak Pal <sayak5913@gmail.com>
2026-05-03 09:48:23 +0530

    Updated

--- scraper.py
+++ scraper.py
@@ -2,9 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+# HEADERS = {
+#     "User-Agent": "Mozilla/5.0",
+#     "Accept-Language": "en-US,en;q=0.9"
+# }
+
 HEADERS = {
-    "User-Agent": "Mozilla/5.0",
-    "Accept-Language": "en-US,en;q=0.9"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "https://www.google.com/",
 }
 
 def safe(soup, selector):
